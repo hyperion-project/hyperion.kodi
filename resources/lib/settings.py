e2544240f1ea4a090a73452660c24ee34a6ad516
Johan <poljvd@gmail.com>
2013-11-01 16:08:45 +0100

    Initial commit

---
+++ resources/lib/settings.py
@@ -0,0 +1,117 @@
+'''
+    XBMC video capturer for Hyperion
+	
+	Copyright (c) 2013 Hyperion Team
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+'''
+import xbmc
+import xbmcaddon
+
+from misc import log
+
+class MyMonitor (xbmc.Monitor):
+	'''Class to capture changes in settings and screensaver state
+	'''
+	
+	def __init__(self, settings):
+		xbmc.Monitor.__init__(self)
+		self.__settings = settings
+		self.__settings.screensaver = xbmc.getCondVisibility("System.ScreenSaverActive")
+		self.__settings.abort = xbmc.abortRequested
+
+	def onAbortRequested(self):
+		self.__settings.abort = False
+		
+	def onSettingsChanged(self):
+		self.__settings.readSettings()
+	  
+	def onScreensaverDeactivated(self):
+		self.__settings.screensaver = False
+	  
+	def onScreensaverActivated(self):    
+		self.__settings.screensaver = True
+
+		
+class MyPlayer (xbmc.Player):
+	'''Class to capture changes in the playing state of XBMC
+	'''
+
+	def __new__(type, settings):
+		return xbmc.Player.__new__(type)
+		
+	def __init__(self, settings):
+		xbmc.Player.__init__(self)
+		self.__settings = settings
+		self.__playbackChanged()
+
+	def __playbackChanged(self):
+		self.__settings.playing      = self.isPlaying()
+		self.__settings.playingVideo = self.isPlayingVideo()
+		self.__settings.playingAudio = self.isPlayingAudio()
+		
+		log("playing = %d video = %d audio = %d" % (self.__settings.playing, self.__settings.playingVideo, self.__settings.playingAudio))
+
+	def onPlayBackStarted(self):
+		self.__playbackChanged()
+
+	def onPlayBackStopped(self):
+		self.__playbackChanged()
+
+	def onPlayBackEnded(self):
+		self.__playbackChanged()
+		
+class Settings:
+	'''Class which contains all addon settings and xbmc state items of interest
+	'''
+	
+	def __init__(self):
+		'''Constructor
+		'''
+		self.rev = 0
+		self.__monitor = MyMonitor(self)
+		self.__player = MyPlayer(self)
+		self.readSettings()
+		
+	def __del__(self):
+		'''Destructor
+		'''
+		del self.__monitor
+		del self.__player
+		
+	def readSettings(self):
+		'''(Re-)read all settings
+		'''
+		log("Reading settings")
+		addon = xbmcaddon.Addon()
+		self.enable = bool(addon.getSetting("hyperion_enable"))
+		self.enableScreensaver = bool(addon.getSetting("screensaver_enable"))
+		self.address = addon.getSetting("hyperion_ip")
+		self.port = int(addon.getSetting("hyperion_port"))
+		self.priority = int(addon.getSetting("hyperion_priority"))
+		self.timeout = int(addon.getSetting("reconnect_timeout"))
+		self.showErrorMessage = True
+		self.rev += 1
+		
+	def grabbing(self):
+		'''Check if we grabbing is requested based on the current state and settings
+		'''
+		return self.enable \
+			and (self.playing and self.playingVideo) \
+			and (self.enableScreensaver or not self.screensaver)
---
+++ resources/lib/state.py
@@ -0,0 +1,154 @@
+'''
+    XBMC video capturer for Hyperion
+	
+	Copyright (c) 2013 Hyperion Team
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy
+	of this software and associated documentation files (the "Software"), to deal
+	in the Software without restriction, including without limitation the rights
+	to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+	copies of the Software, and to permit persons to whom the Software is
+	furnished to do so, subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in
+	all copies or substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+	IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+	FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+	AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+	LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+	THE SOFTWARE.
+'''
+import xbmc
+import xbmcaddon
+
+from hyperion.Hyperion import Hyperion
+from misc import log
+from misc import notify
+
+class DisconnectedState:
+	'''
+	Default state class when disconnected from the Hyperion server
+	'''
+	
+	def __init__(self, settings):
+		'''Constructor
+			- settings: Settings structure
+		'''
+		log("Entering disconnected state")
+		self.__settings = settings
+		
+	def execute(self):
+		'''Execute the state
+			- return: The new state to execute
+		'''
+		# check if we are enabled
+		if not self.__settings.grabbing():
+			xbmc.sleep(500)
+			return self
+			
+		# we are enabled and want to advance to the connected state
+		try:
+			nextState = ConnectedState(self.__settings)
+			return nextState
+		except Exception, e:
+			# unable to connect. notify and go to the error state
+			if self.__settings.showErrorMessage:
+				notify(xbmcaddon.Addon().getLocalizedString(32100))
+				self.__settings.showErrorMessage = False
+				
+			# continue in the error state
+			return ErrorState(self.__settings)
+
+class ConnectedState:
+	'''
+	State class when connected to Hyperion and grabbing video
+	'''
+
+	def __init__(self, settings):
+		'''Constructor
+			- settings: Settings structure
+		'''
+		log("Entering connected state")
+
+		self.__settings = settings
+		self.__hyperion = None
+		self.__capture = None
+		
+		# try to connect to hyperion
+		self.__hyperion = Hyperion(self.__settings.address, self.__settings.port)
+		
+		# create the capture object
+		self.__capture = xbmc.RenderCapture()
+		self.__capture.capture(64, 64, xbmc.CAPTURE_FLAG_CONTINUOUS)
+		
+	def __del__(self):
+		'''Destructor
+		'''
+		del self.__hyperion
+		del self.__capture
+		
+	def execute(self):
+		'''Execute the state
+			- return: The new state to execute
+		'''
+		# check if we still need to grab
+		if not self.__settings.grabbing():
+			# return to the disconnected state
+			return DisconnectedState(self.__settings)
+		
+		# capture an image
+		self.__capture.waitForCaptureStateChangeEvent(200)
+		if self.__capture.getCaptureState() == xbmc.CAPTURE_STATE_DONE:
+			# retrieve image data and reformat into rgb format
+			data = self.__capture.getImage()
+			if self.__capture.getImageFormat() == 'ARGB':
+				del data[0::4]
+			elif self.__capture.getImageFormat() == 'BGRA':
+				del data[3::4]
+				data[0::3], data[2::3] = data[2::3], data[0::3]
+
+			try:
+				#send image to hyperion
+				self.__hyperion.sendImage(self.__capture.getWidth(), self.__capture.getHeight(), str(data), self.__settings.priority, 500)
+			except Exception, e:
+				# unable to send image. notify and go to the error state
+				notify(xbmcaddon.Addon().getLocalizedString(32101))
+				return ErrorState(self.__settings)
+				
+		xbmc.sleep(100)
+			
+		return self
+			
+class ErrorState:
+	'''
+	State class which is activated upon an error
+	'''
+
+	def __init__(self, settings):
+		'''Constructor
+			- settings: Settings structure
+		'''
+		log("Entering error state")
+		self.__settings = settings
+		
+	def execute(self):
+		'''Execute the state
+			- return: The new state to execute
+		'''
+		# take note of the current revision of the settings
+		rev = self.__settings.rev
+		
+		#stay in error state for the specified timeout or until the settings have been changed
+		i = 0
+		while (i < self.__settings.timeout) and (rev == self.__settings.rev):
+			if self.__settings.abort:
+				return self
+			else:
+				xbmc.sleep(1000)
+			i += 1
+		
+		# continue in the disconnected state
+		return DisconnectedState(self.__settings)
